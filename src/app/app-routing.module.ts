26112497c0a7cf2b6fe06ddb3a40f7388f1367e6
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2026-06-29 10:49:44 -0600

    ad comment in angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-
+// This file defines the routing configuration for the Angular application. It imports necessary modules and sets up routes for different parts of the application, including lazy loading for the 'auth' and 'dashboard' modules, and applies route guards to protect certain routes.
 const routes: Routes = [
   {
     path: 'auth',
